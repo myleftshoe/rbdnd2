028f9867685ed1387cc375aa954a2eb4e2619f26
Paul <paulesammut@gmail.com>
2018-10-16 20:22:55 +1100

    Updates

--- precache-manifest.20f17142e1b26378ee5dd5d3239941ed.js
+++ precache-manifest.20f17142e1b26378ee5dd5d3239941ed.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rbdnd2/static/js/runtime~main.b146121f.js"
   },
   {
-    "revision": "952e27bdd68ea1e115ea",
-    "url": "/rbdnd2/static/js/main.952e27bd.chunk.js"
+    "revision": "a92c8c2c94e86adcacd5",
+    "url": "/rbdnd2/static/js/main.a92c8c2c.chunk.js"
   },
   {
     "revision": "8266fa469b0e2284e08e",
     "url": "/rbdnd2/static/js/1.8266fa46.chunk.js"
   },
   {
-    "revision": "952e27bdd68ea1e115ea",
-    "url": "/rbdnd2/static/css/main.cb222e11.chunk.css"
+    "revision": "a92c8c2c94e86adcacd5",
+    "url": "/rbdnd2/static/css/main.a881f28c.chunk.css"
   },
   {
-    "revision": "a86214d9fbb16c0fa68e6104cad236ad",
+    "revision": "acbee787419a9b2aea562614b2a4a37b",
     "url": "/rbdnd2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/rbdnd2/precache-manifest.96bd801bc3f7118cfdd110b3d303f36f.js"
+  "/rbdnd2/precache-manifest.20f17142e1b26378ee5dd5d3239941ed.js"
 );
 
 workbox.clientsClaim();
