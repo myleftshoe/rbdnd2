fd5fd60543efda18da565f279b2f91e6c000f042
Paul <paulesammut@gmail.com>
2018-10-19 18:55:58 +1100

    Updates

--- precache-manifest.b118e808bca763662fcecb7ab6db9507.js
+++ precache-manifest.b118e808bca763662fcecb7ab6db9507.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rbdnd2/static/js/runtime~main.b146121f.js"
   },
   {
-    "revision": "d7769c8e56fca22a0fde",
-    "url": "/rbdnd2/static/js/main.d7769c8e.chunk.js"
+    "revision": "fcc8ec2a7591da9fc5c6",
+    "url": "/rbdnd2/static/js/main.fcc8ec2a.chunk.js"
   },
   {
     "revision": "8266fa469b0e2284e08e",
     "url": "/rbdnd2/static/js/1.8266fa46.chunk.js"
   },
   {
-    "revision": "d7769c8e56fca22a0fde",
-    "url": "/rbdnd2/static/css/main.9f2dabb8.chunk.css"
+    "revision": "fcc8ec2a7591da9fc5c6",
+    "url": "/rbdnd2/static/css/main.ab41b561.chunk.css"
   },
   {
-    "revision": "49715f34d43fc65fc86bebc7a6a98a4a",
+    "revision": "3c1f8b95f4cb74269a56db6b9d51917d",
     "url": "/rbdnd2/index.html"
   }
 ];
